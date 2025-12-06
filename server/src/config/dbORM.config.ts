181f59cc53e0966ec284a836abb90e98371cc0d7
Nabeen-Thapa <nabeen2058@gmail.com>
2025-12-06 15:31:54 +0545

    recorrec database password

--- server/src/config/dbORM.config.ts
+++ server/src/config/dbORM.config.ts
@@ -17,7 +17,7 @@ export const falfulConnection = new DataSource({
     host : process.env.DB_HOST,
     port: 5432,
     username :process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORDd,
+    password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME,  
     synchronize: true,
     logging: false,
