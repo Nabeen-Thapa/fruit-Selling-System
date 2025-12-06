2209f4eeb4066c0e7de573a85e65619fe3fbe3f7
Nabeen-Thapa <nabeen2058@gmail.com>
2025-12-06 19:23:36 +0545

    add vite-env.d.ts

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  // Add more environment variables here if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
